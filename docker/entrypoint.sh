bc84f915bb830136c1734c83cf953b6e982e8502
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2024-11-28 11:16:47 -0300

    [add] iperf common flags beign used

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -10,11 +10,13 @@ tc qdisc add dev eth0 root handle 1: htb default 1
 tc class add dev eth0 parent 1: classid 1:1 htb rate ${NETWORK_SPEED}mbit burst 15k
 tc filter add dev eth0 protocol ip parent 1: prio 1 handle 1: cgroup
 
+IPERF_COMMON_FLAGS="-w 256K -P 10 -t 10 --get-server-output"
+
 IPERF_COMMANDS=(
-  "iperf3 -c iperf-server -w 256K -M 1500 -t 10 > /volumes/iperf/${NETWORK_SPEED}-1500-tcp.txt 2> /dev/null"
-  "iperf3 -c iperf-server -w 256K -M 9000 -t 10 > /volumes/iperf/${NETWORK_SPEED}-9000-tcp.txt 2> /dev/null"
-  "iperf3 -c iperf-server -w 256K -M 1500 -t 10 -u -b ${NETWORK_SPEED}M > /volumes/iperf/${NETWORK_SPEED}-1500-udp.txt 2> /dev/null"
-  "iperf3 -c iperf-server -w 256K -M 9000 -t 10 -u -b ${NETWORK_SPEED}M > /volumes/iperf/${NETWORK_SPEED}-9000-udp.txt 2> /dev/null"
+  "iperf3 -c iperf-server ${IPERF_COMMON_FLAGS} -M 1500 > /volumes/iperf/${NETWORK_SPEED}-1500-tcp.txt 2> /dev/null"
+  "iperf3 -c iperf-server ${IPERF_COMMON_FLAGS} -M 9000 > /volumes/iperf/${NETWORK_SPEED}-9000-tcp.txt 2> /dev/null"
+  "iperf3 -c iperf-server ${IPERF_COMMON_FLAGS} -M 1500 -u -b ${NETWORK_SPEED}M > /volumes/iperf/${NETWORK_SPEED}-1500-udp.txt 2> /dev/null"
+  "iperf3 -c iperf-server ${IPERF_COMMON_FLAGS} -M 9000 -u -b ${NETWORK_SPEED}M > /volumes/iperf/${NETWORK_SPEED}-9000-udp.txt 2> /dev/null"
 )
 
 # try to run iperf3 commands until all of them are successful
